937174b01d6a9cc610adc759c53977d686e706b1
Randy <rpitcher@purdue.edu>
2015-09-26 09:49:58 -0400

    Cleaned up slider content

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $( document ).ready(function(){
 	$(".button-collapse").sideNav();
-	$('.slider').slider({full_width: true});
+	$('.slider').slider({full_width: true, height: 600});
 	 $('#modal1').openModal();
 })
